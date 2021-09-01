ba9f5b6951b09856d0c292c8e117f668f432785c
lutfor779 <mdlrrahman779@gmail.com>
2021-09-02 04:21:18 +0600

    unwanted items removed

--- js/api.js
+++ js/api.js
@@ -1,20 +1,22 @@
- // search
+// search
 const getData = () => {
     document.getElementById('search-result').textContent = '';
     document.getElementById('info').textContent = '';
     let search = document.getElementById('search-text');
+
+    // check empty data
     if (search.value == '') {
         document.getElementById('spinner').style.display = 'none';
         const div = document.createElement('div');
-        div.classList.add('text-center', 'fs-1', 'text-primary');
-        div.innerText = 'Write what you want to show.';
+        div.classList.add('text-center', 'fs-2', 'fw-bold', 'text-success');
+        div.innerText = 'Write something to show.';
         document.getElementById('search-result').appendChild(div);
     }
     else {
         const url = `http://openlibrary.org/search.json?q=${search.value}`;
-            fetch(url)
+        fetch(url)
             .then(res => res.json())
-                .then(data => displayResults(data));
+            .then(data => displayResults(data));
         document.getElementById('spinner').style.display = 'block';
     }
     search.value = '';
@@ -25,10 +27,12 @@ const getData = () => {
 const displayResults = results => {
     document.getElementById('spinner').style.display = 'none';
     let i = 0;
+
+    // check items found
     if (results.numFound == 0) {
         document.getElementById('spinner').style.display = 'none';
         const div = document.createElement('div');
-        div.classList.add('text-center', 'fs-1', 'fw-bold', 'text-danger');
+        div.classList.add('text-center', 'fs-2', 'fw-bold', 'text-danger');
         div.innerText = 'No results found!';
         document.getElementById('search-result').appendChild(div);
         document.getElementById('info').style.display = 'none';
@@ -37,23 +41,26 @@ const displayResults = results => {
     else {
         results.docs.forEach(result => {
             const url = `https://covers.openlibrary.org/b/id/${result.cover_i}-M.jpg`;
-            
-            const div = document.createElement('div');
-            div.classList.add('col');
-            div.innerHTML = `
+
+            // delete without cover item
+            if (result.cover_i) {
+                const div = document.createElement('div');
+                div.classList.add('col');
+                div.innerHTML = `
                 <div class="card h-100">
-                    <img src="${result.cover_i ? url : ''}"
+                    <img src="${url}"
                         height = "350px"
                         class="card-img-top"
                            alt="No Image found">
                         <div class="card-body">
                             <h3 class="card-title">${result.title}</h3>
                             <p>${result.author_name ? result.author_name : ''}</p>
-                            <p>${result.first_publish_year ? result.first_publish_year: ''}</p>
+                            <p>${result.first_publish_year ? result.first_publish_year : ''}</p>
                         </div>
                 </div>`;
-            document.getElementById('search-result').appendChild(div);
-            i++;        
+                document.getElementById('search-result').appendChild(div);
+                i++
+            }
         })
     }
     itemsFound(i, results);
@@ -62,7 +69,7 @@ const displayResults = results => {
 // display Items found
 const itemsFound = (items, total) => {
     const div = document.createElement('div');
-    div.classList.add('text-center', 'fs-3', 'text-danger', 'border', 'border-danger', 'rounded-3', 'w-25', 'mx-auto');
+    div.classList.add('text-center', 'fs-3', 'text-success', 'border', 'border-danger', 'rounded-3', 'w-25', 'mx-auto');
     div.innerText = `${items} of ${total.numFound} results`;
     document.getElementById('info').appendChild(div);
 }
